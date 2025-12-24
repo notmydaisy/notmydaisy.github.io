a4d736798b01494aeb55f480f584b831e3fb6d66
notmydaisy <belphegor.rb132@gmail.com>
2025-12-25 00:36:37 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,18 @@
-print("")
+--[[
+    Jujutsu Zero - 6760085372
+    Anime Final Quest - 7798947148
+    The Forge - 7671049560
+    Brainrot Seas - 9048562273
+]]
+
+local GameID = game.GameId
+
+if GameID == 6760085372 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3186a4f30fcd853ae613dcbee2534612.lua"))()
+elseif GameID == 7798947148 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/984cd0fac08abc8c1f8b9eeab9125263.lua"))()
+elseif GameID == 7671049560 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/38d7e491ea3a0f6085bebbb240f1d340.lua"))()
+elseif GameID == 9048562273 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef21065570a097507c1a32748ff28841.lua"))()
+end
