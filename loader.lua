f6203c594a200aac026226f4be4dbc02d3114073
notmydaisy <belphegor.rb132@gmail.com>
2026-01-03 20:31:02 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,4 +15,6 @@ elseif GameID == 7671049560 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/38d7e491ea3a0f6085bebbb240f1d340.lua"))()
 elseif GameID == 9048562273 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef21065570a097507c1a32748ff28841.lua"))()
+elseif GameID == 7440311707 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d21613ac160cc5471dcbdff8f466f350.lua"))()
 end
