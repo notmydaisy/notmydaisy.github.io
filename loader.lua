25ae96e9a600e84e92cb15f3c6ed00f06207e7ac
notmydaisy <belphegor.rb132@gmail.com>
2026-01-30 15:53:17 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,7 @@
     Anime Final Quest - 7798947148
     The Forge - 7671049560
     Brainrot Seas - 9048562273
+    Tap Simulator - 8779464785
 ]]
 
 local GameID = game.GameId
@@ -17,4 +18,6 @@ elseif GameID == 9048562273 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef21065570a097507c1a32748ff28841.lua"))()
 elseif GameID == 7440311707 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d21613ac160cc5471dcbdff8f466f350.lua"))()
+elseif GameID == 8779464785 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ba13749542c8fa8ca8cfd9220fb44b5d.lua"))()
 end
